62b94779f8e06d3672486c8052c1308ab0077fc9
Paweł Kwieciński <pa.kwiecinski96@gmail.com>
2019-03-02 12:48:18 +0100

    Summer-Spring

---
+++ src/main/java/pl/teb/edukacja/summerproject/SummerprojectApplication.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.summerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerprojectApplication.class, args);
+	}
+
+}
